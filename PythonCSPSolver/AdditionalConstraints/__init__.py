8fd9df7586f9775592dece64f5578c29979943e2
Emre ArapcicUevak <90074488+EmreArapcicUevak@users.noreply.github.com>
2025-10-24 21:47:29 +0200

    Change the __init__.py to include the other constraints, and rename the kings move .py file

--- PythonCSPSolver/AdditionalConstraints/__init__.py
+++ PythonCSPSolver/AdditionalConstraints/__init__.py
@@ -1,1 +1,7 @@
 from .white_dot_constraint import *
+from .black_dot_constraint import *
+from .kings_move_constraint import *
+from .knights_move_constraint import *
+from .german_whisper_constraint import *
+from .thermo_constraint import *
+from .sudoku_x_constraint import *
